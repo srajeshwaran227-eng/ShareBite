4851da59ea93953e3b9445431322b5237c4bd56b
ShareBite Dev <sharebite@example.com>
2026-03-09 15:03:28 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ShareBite/sw.js', { scope: '/ShareBite/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
